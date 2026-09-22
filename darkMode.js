c88d6f9dc837fb3d9cf0e7474503ccc0e74b73cb
Beni diktus Guintolin <guinhendri@gmail.com>
2026-09-22 21:52:04 +0700

    feat: add like functionality with localStorage

--- darkMode.js
+++ darkMode.js
@@ -2,32 +2,27 @@ const darkModeBtn = document.getElementById("darkModeBtn");
 const navbar = document.getElementById("navbar");
 
 darkModeBtn.addEventListener("click", function () {
+  document.body.classList.toggle("bg-gray-100");
+  document.body.classList.toggle("bg-gray-900");
 
-    // Mengubah background dan warna teks body
-    document.body.classList.toggle("bg-gray-100");
-    document.body.classList.toggle("bg-gray-900");
+  document.body.classList.toggle("text-gray-900");
+  document.body.classList.toggle("text-white");
 
-    document.body.classList.toggle("text-gray-900");
-    document.body.classList.toggle("text-white");
+  navbar.classList.toggle("bg-white");
+  navbar.classList.toggle("bg-gray-800");
 
-    // Mengubah warna navbar
-    navbar.classList.toggle("bg-white");
-    navbar.classList.toggle("bg-gray-800");
+  navbar.classList.toggle("text-gray-900");
+  navbar.classList.toggle("text-white");
 
-    navbar.classList.toggle("text-gray-900");
-    navbar.classList.toggle("text-white");
+  darkModeBtn.classList.toggle("bg-gray-900");
+  darkModeBtn.classList.toggle("bg-yellow-400");
 
-    // Mengubah tampilan tombol
-    darkModeBtn.classList.toggle("bg-gray-900");
-    darkModeBtn.classList.toggle("bg-yellow-400");
+  darkModeBtn.classList.toggle("text-white");
+  darkModeBtn.classList.toggle("text-gray-900");
 
-    darkModeBtn.classList.toggle("text-white");
-    darkModeBtn.classList.toggle("text-gray-900");
-
-    // Mengubah teks tombol
-    if (document.body.classList.contains("bg-gray-900")) {
-        darkModeBtn.innerHTML = "☀️ Light Mode";
-    } else {
-        darkModeBtn.innerHTML = "🌙 Dark Mode";
-    }
+  if (document.body.classList.contains("bg-gray-900")) {
+    darkModeBtn.innerHTML = "☀️ Light Mode";
+  } else {
+    darkModeBtn.innerHTML = "🌙 Dark Mode";
+  }
 });
